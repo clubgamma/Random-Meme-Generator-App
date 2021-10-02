87b87dfee45ea526daa6bc81be33e8124e3a9746
KeneePatel <patelkenee2804@gmail.com>
2021-10-02 12:23:10 +0530

    successfully added share functionality

--- app/src/main/java/io/github/clubgamma/randommemegenerator/MainActivity.java
+++ app/src/main/java/io/github/clubgamma/randommemegenerator/MainActivity.java
@@ -3,16 +3,36 @@ package io.github.clubgamma.randommemegenerator;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaring statements for the share functionality
+    BitmapDrawable drawable;
+    Bitmap bitmap;
+    ImageView img;
+    Button shareBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Define ShareButton and image object here
+        shareBtn = findViewById(R.id.share);
+        img = findViewById(R.id.imageView);
 
         // Define ActionBar object
         ActionBar actionBar;
@@ -26,5 +46,34 @@ public class MainActivity extends AppCompatActivity {
 
         // Set BackgroundDrawable
         ((ActionBar) actionBar).setBackgroundDrawable(colorDrawable);
+
+        shareBtn.setOnClickListener(v -> shareImage());
+    }
+
+    // Method responsible for getting image from the imageView and sharing it.
+    private void shareImage() {
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        drawable = (BitmapDrawable) img.getDrawable();
+        bitmap = drawable.getBitmap();
+        File file = new File(getExternalCacheDir()+"/"+"meme"+".png");
+        Intent shareIntent;
+        
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("image/*");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+            shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        startActivity(Intent.createChooser(shareIntent, "Share using "));
     }
 }
