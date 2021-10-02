8a21c032276b71ca7cae7b9c5731af021c864787
Ansh <chinrust@gmail.com>
2021-10-02 17:53:06 +0530

    Get image from api and display

--- app/src/main/java/io/github/clubgamma/randommemegenerator/MainActivity.java
+++ app/src/main/java/io/github/clubgamma/randommemegenerator/MainActivity.java
@@ -6,13 +6,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.github.clubgamma.randommemegenerator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
+    String url = "https://meme-api.herokuapp.com/gimme";
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+        getMemeImage();
 
         // Define ActionBar object
         ActionBar actionBar;
@@ -26,5 +49,37 @@ public class MainActivity extends AppCompatActivity {
 
         // Set BackgroundDrawable
         ((ActionBar) actionBar).setBackgroundDrawable(colorDrawable);
+        activityMainBinding.next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getMemeImage();
+            }
+        });
+    }
+
+    public void getMemeImage() {
+        queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String memeUrl = null;
+                try {
+                    memeUrl = response.getString("url");
+                    loadImageIntoImageView(memeUrl);
+                } catch (JSONException jsonException) {
+                    jsonException.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(jsonObjectRequest);
+    }
+
+    public void loadImageIntoImageView(String url) {
+        Glide.with(MainActivity.this).load(url).into(activityMainBinding.imageView);
     }
 }
